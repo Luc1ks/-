87759c6cce300cb36f7f9c068fbf434620fb3afa
DKprofile <denkurilin200@gmail.com>
2020-07-07 19:50:09 +0300

    build added

---
+++ build/precache-manifest.ba75b7b90e68e0d6516e0aa7161fd853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4f5001bae2002473003127eeccb5b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2700667ae099ef15d17",
+    "url": "/static/css/main.f055112e.chunk.css"
+  },
+  {
+    "revision": "5f3eb2b254a43f1ba7e8",
+    "url": "/static/js/2.2f7dadac.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.2f7dadac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2700667ae099ef15d17",
+    "url": "/static/js/main.6f7ec051.chunk.js"
+  },
+  {
+    "revision": "8ea716fe977ed637fa59",
+    "url": "/static/js/runtime-main.733e8b81.js"
+  }
+]);
